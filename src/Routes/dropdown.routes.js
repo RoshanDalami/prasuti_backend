7c5b4b15c26f4d2905b1e410497e5dec74db2475
aakash-22ak <akashshrestha678@gmail.com>
2024-04-04 16:40:16 +0545

    changes

--- src/Routes/dropdown.routes.js
+++ src/Routes/dropdown.routes.js
@@ -1,14 +1,21 @@
-import express from 'express';
-import { getBabyStatus,getBabyTransfer,getBreastFeeding,getDelivery,getEthnicity,getGestational,getParity  } from '../Controller/dropDown.controller.js'
+import express from "express";
+import {
+  getBabyStatus,
+  getBabyTransfer,
+  getBreastFeeding,
+  getDelivery,
+  getEthnicity,
+  getGestational,
+  getParity,
+} from "../Controller/dropDown.controller.js";
 const DropdownRoute = express.Router();
 
-DropdownRoute.route('/getBabyStatus').get(getBabyStatus)
-DropdownRoute.route('/getBabyTransfer').get(getBabyTransfer)
-DropdownRoute.route('/getBreastFeeding').get(getBreastFeeding)
-DropdownRoute.route('/getDelivery').get(getDelivery)
-DropdownRoute.route('/getEthnicity').get(getEthnicity)
-DropdownRoute.route('/getGestational').get(getGestational)
-DropdownRoute.route('/getParity').get(getParity)
+DropdownRoute.route("/getBabyStatus").get(getBabyStatus);
+DropdownRoute.route("/getBabyTransfer").get(getBabyTransfer);
+DropdownRoute.route("/getBreastFeeding").get(getBreastFeeding);
+DropdownRoute.route("/getDelivery").get(getDelivery);
+DropdownRoute.route("/getEthnicity").get(getEthnicity);
+DropdownRoute.route("/getGestational").get(getGestational);
+DropdownRoute.route("/getParity").get(getParity);
 
-
-export {DropdownRoute}
+export { DropdownRoute };
